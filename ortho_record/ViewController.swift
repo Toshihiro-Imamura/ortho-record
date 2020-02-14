f1a2e1d8b6b58511399593f8aed7cc3affb570c3
toshihiro.imamura <imamaura.orts@gmail.com>
2020-02-14 12:23:56 +0900

    Initial Commit

---
+++ ortho_record/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ortho_record
+//
+//  Created by 今村俊博 on 2020/02/14.
+//  Copyright © 2020 toshihiro.imamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
